b6d5e0e411d6ee0fe0b33e575b031e0fa6015161
Jansan <jansan-26@hotmail.com>
2020-09-02 13:50:55 +0200

    validation

--- Personregister/Person.cs
+++ Personregister/Person.cs
@@ -1,32 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Personregister
 {
     class Person
     {
+       
         private string firstName;
-
+        [Required(ErrorMessage = "Firstname is required.")]
         public string FirstName
         {
-            get { return firstName; }
-            set { firstName = value; }
+            get 
+            {
+                return firstName; 
+            }
+            set 
+            {
+                //validate the input
+                if (string.IsNullOrEmpty(value))
+                
+                    throw new ArgumentNullException("FirstName cannot be null");
+
+                if (value.Length < 2 && value.Length < 10)
+
+                    throw new ArgumentOutOfRangeException("FirstName cannot be minst 2 and longer than 10 ");
+                
+                firstName = value;
+            }
         }
-
+       
         private string lastName;
-
+        [Required(ErrorMessage = "LastName cannot be null")]
         public string LastName
         {
-            get { return lastName; }
-            set { lastName = value; }
+            get
+            { 
+                return lastName; 
+            }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                
+                    throw new ArgumentNullException("Lastname cannot be null");
+                if (value.Length < 3 && value.Length < 15)
+                    throw new ArgumentOutOfRangeException("LastName cannot be minst 2 and longer than 15");
+               
+                lastName = value;
+            }
         }
         private int age;
 
         public int Age
         {
             get { return age; }
-            set { age = value; }
+            set
+            {
+                if (age > 0)
+                    throw new ArgumentException("Age can only be greater than 0");
+                age = value;
+            }
         }
         private float height;
 
