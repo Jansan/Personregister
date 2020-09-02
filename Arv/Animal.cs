010d1609a4d145f63098f80996bac918f6f416f7
Jansan <jansan-26@hotmail.com>
2020-09-02 16:53:08 +0200

    create new project and Animal class created

---
+++ Arv/Animal.cs
@@ -0,0 +1,40 @@
+﻿namespace Arv
+{
+    abstract class Animal
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private double weight;
+
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public Animal(string name, double weight, int age)
+        {
+            Name = name;
+            Weight = weight;
+            Age = age;
+        }
+
+        public abstract void DoSound();
+       
+
+    }
+}
