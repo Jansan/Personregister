7a05de9e52cbfbc62666f747c9c8485aaf0fb3a9
Jansan <jansan-26@hotmail.com>
2020-09-04 14:44:18 +0200

    TextInputError class

---
+++ Polymorfism/TextInputError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorfism
+{
+    class TextInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return $"You tried to use a text input in a numericonly field an error!";
+        }
+    }
+}
